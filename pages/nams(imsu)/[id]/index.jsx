c379a7b975554620b7ccd5942c9521301fd8ee02
benjamin <deeprising4ilife@gmail.com>
2024-01-28 11:30:30 -0800

    first commit

---
+++ pages/nams(imsu)/[id]/index.jsx
@@ -0,0 +1,113 @@
+import artiii from '../../../styles/Article.module.css'
+import Link from 'next/link'
+
+
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+
+  return (
+    <>
+    <div className={artiii.body}>
+      <div className={artiii.userprofile}>
+      <div className={artiii.profiletop}>
+                <img className={artiii.img} src={article.picturepath}alt="picturepath"/>
+                <div className={artiii.profileinfo}>
+                <div className={artiii.profileinfoo}>
+                        <h2 >{article.inName}</h2>  
+                        <h1>--REG NO:{article.reg}--
+                        </h1>
+                        </div>
+                    </div>
+                
+                </div>
+                <div className={artiii.profilebottom}>
+                <div className={artiii.school}> 
+                <div className={artiii.profileinfo}> 
+                <div className={artiii.profileinfooo}>
+                                <h1>- IMO STATE UNIVERSITY -</h1>
+                                 <h3>- MICROBIOLOGY STUDENTS ASSOCIATION -</h3>
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={artiii.profilebottom}>
+                    <div className={artiii.flecy}>
+                    <div className={artiii.bg}>
+                                <h1 >B/G</h1>
+                                <div className={artiii.profileinfo}>
+                                <h1 >{article.bloodgroup}</h1>
+                                </div>
+                            </div>
+                            <div className={artiii.status}>
+                            <h1 >STATUS/VALIDITY</h1>
+                            <div className={artiii.profileinfo}>
+                            <div className={artiii.col}>
+                                    <h1>{article.Status}</h1>
+                                    <h4 >- {article.validity} -</h4>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div className={artiii.sex}>
+                                <h1 >SEX</h1>
+                                <div className={artiii.profileinfo}>
+                                <h1 >{article.sex}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={artiii.lga}>
+                        <h1>LGA/STATE OF ORIGIN</h1>
+                        
+                        <div className={artiii.profileinfo}> 
+                        <div className={artiii.lgacol}>
+                                <h1 >- {article.state} -</h1>
+                                <h3>- {article.localgovt} -</h3>
+                                </div>
+                                 </div>
+                            </div>
+                        
+                            <div className={artiii.bottom}>
+                            <div className={artiii.contact}>
+                                <h1>CONTACT:</h1>
+                                <div className={artiii.profileinfo}>
+                                    <>
+                                    <div className={artiii.p1}>
+                                            <p2>{article.inparentno}</p2>
+                                        </div>
+                                    </>                   
+                                </div>
+                            </div>
+                            <div className={artiii.contact}>
+                                <h1>EMERGENCY CONTACT:</h1>
+                                <div className={artiii.profileinfo}>
+                                    <>
+                                    <div className={artiii.p2}>
+                                            <p2>{article.inparentno2}</p2>
+                                        </div>
+                                    </>                   
+                                </div>                           
+                            </div>
+                        </div>
+                    </div>       
+                </div>
+                </div>
+    </>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://erin-inquisitive-hare.cyclic.cloud/api/products/${context.params.id}` )
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+      fallback:false
+    }
+   
+  }
+}
+
+export default article
