d738d8e3ce1bc21d3a67f6a90d4586e340c11494
benjamin <deeprising4ilife@gmail.com>
2024-06-21 00:03:22 -0800

    first commit

--- pages/FUTOSUG/[id]/index.jsx
+++ pages/FUTOSUG/[id]/index.jsx
@@ -11,21 +11,12 @@ const article = ({ article }) => {
     <div className={artiii.body}>
         <div className={artiii.userprofile}>
             <div className={artiii.profiletop}>
-<<<<<<< HEAD
                     <img className={artiii.img} src={article.po}alt="picturepath"/>
                     <div className={artiii.profileinfo}>
                         <div className={artiii.profileinfoo}>
                             <h2 >{article.Aname.Name}</h2>  
                             <h3 >{article.Aname.Mname}    {article.Aname.Surname}</h3>
                             <h1>--REG:{article.RegNo}--
-=======
-                    <img className={artiii.img} src={article.picturepath}alt="picturepath"/>
-                    <div className={artiii.profileinfo}>
-                        <div className={artiii.profileinfoo}>
-                            <h2 >{article.inNamer.Name}</h2>  
-                            <h3 >{article.inNamer.Mname}    {article.inNamer.Surname}</h3>
-                            <h1>--REG:{article.reg}--
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                             </h1>
                         </div>
                     </div>
@@ -45,11 +36,7 @@ const article = ({ article }) => {
                         <   div className={artiii.profileinfo}> 
                                 <div className={artiii.lgacol}>
                                     <h1 >- {article.Faculty} -</h1>
-<<<<<<< HEAD
                                     <h3>- {article.Dept} -</h3>
-=======
-                                    <h3>- {article.Department} -</h3>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                 </div>
                                  </div>
                             </div>
@@ -60,11 +47,7 @@ const article = ({ article }) => {
                     <div className={artiii.bg}>
                                 <h1 >B/G</h1>
                                 <div className={artiii.profileinfo}>
-<<<<<<< HEAD
                                 <h1 >{article.Bloodgroup}</h1>
-=======
-                                <h1 >{article.bloodgroup}</h1>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                 </div>
                             </div>
                             <div className={artiii.status}>
@@ -72,11 +55,7 @@ const article = ({ article }) => {
                             <div className={artiii.profileinfo}>
                             <div className={artiii.col}>
                                     <h1>{article.Status}</h1>
-<<<<<<< HEAD
                                     <h4 >- {article.Validity} -</h4>
-=======
-                                    <h4 >- {article.validity} -</h4>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                 </div>
                                 </div>
                             </div>
@@ -84,11 +63,7 @@ const article = ({ article }) => {
                             <div className={artiii.sex}>
                                 <h1 >SEX</h1>
                                 <div className={artiii.profileinfo}>
-<<<<<<< HEAD
                                 <h1 >{article.Sex}</h1>
-=======
-                                <h1 >{article.sex}</h1>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                 </div>
                             </div>
                         </div>
@@ -97,13 +72,8 @@ const article = ({ article }) => {
                         
                         <div className={artiii.profileinfo}> 
                         <div className={artiii.lgacol}>
-<<<<<<< HEAD
                                 <h1 >- {article.State} -</h1>
                                 <h3>- {article.LocalGovt} -</h3>
-=======
-                                <h1 >- {article.state} -</h1>
-                                <h3>- {article.localgovt} -</h3>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                 </div>
                                  </div>
                             </div>
@@ -114,11 +84,7 @@ const article = ({ article }) => {
                                 <div className={artiii.profileinfo}>
                                     <>
                                     <div className={artiii.p1}>
-<<<<<<< HEAD
                                             <p2>{article.PhoneNo}</p2>
-=======
-                                            <p2>{article.inparentno}</p2>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                         </div>
                                     </>                   
                                 </div>
@@ -128,11 +94,7 @@ const article = ({ article }) => {
                                 <div className={artiii.profileinfo}>
                                     <>
                                     <div className={artiii.p2}>
-<<<<<<< HEAD
                                             <p2>{article.EmergencyNo}</p2>
-=======
-                                            <p2>{article.inparentno2}</p2>
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
                                         </div>
                                     </>                   
                                 </div>                           
@@ -159,8 +121,4 @@ export const getServerSideProps = async (context) => {
   }
 }
 
-<<<<<<< HEAD
 export default article
-=======
-export default article
->>>>>>> 184085085fb28f393264c04cab1a04ceb346c1c1
