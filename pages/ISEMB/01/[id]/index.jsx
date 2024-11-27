3eeb38faf78c9864524e6d747d4d61341799f3c9
benjamin edigbe <deeprising4ilife@gmail.com>
2024-11-27 01:21:38 -0800

    first commit

---
+++ pages/ISEMB/01/[id]/index.jsx
@@ -0,0 +1,123 @@
+import artiii from '../../../../styles/Article.module.css'
+import Link from 'next/link'
+
+
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+
+  return (
+    <>
+    <div className={artiii.body}>
+        <div className={artiii.userprofile}>
+            <div className={artiii.profiletop}>
+                    <img className={artiii.img} src={article.picturepath}alt="picturepath"/>
+                    <div className={artiii.profileinfo}>
+                        <div className={artiii.profileinfoo}>
+                            <h2 >{article.Aname.Name}</h2>  
+                            <h3 >{article.Aname.Mname}    {article.Aname.Surname}</h3> 
+                            <h1>--NIN:{article.NIN}--
+                            </h1>
+                        </div>
+                    </div>
+            </div>
+                    <div className={artiii.profilebottom}>
+                        <div className={artiii.school}>
+                            <div className={artiii.profileinfo}> 
+                                <div className={artiii.profileinfooo}>
+                                    <h1>- EBENATOR EKWE SECONDARY-</h1>
+                                    <h3>- SCHOOL, ISU IMO STATE -</h3>
+                                 </div>
+                            </div>
+                        </div>
+                        <div className={artiii.lga}>
+                        <h1>DATE OF BIRTH/PRESENT CLASS</h1>
+                        
+                        <div className={artiii.profileinfo}> 
+                        <div className={artiii.lgacol}>
+                                <h1 >- {article.DateofBirth} -</h1>
+                                <h3>- {article.Presentclass} -</h3>
+                                </div>
+                                 </div>
+                            </div>
+                    </div>
+                    <div className={artiii.profilebottom}>
+                    <div className={artiii.flecy}>
+                    <div className={artiii.bg}>
+                                <h1 >B/G</h1>
+                                <div className={artiii.profileinfo}>
+                                <h1 >{article.Bloodgroup}</h1>
+                                </div>
+                            </div>
+                            <div className={artiii.status}>
+                            <h1 >STATUS/VALIDITY</h1>
+                            <div className={artiii.profileinfo}>
+                            <div className={artiii.col}>
+                                    <h1>{article.Status}</h1>
+                                    <h4 >- {article.Validity} -</h4>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div className={artiii.sex}>
+                                <h1 >SEX</h1>
+                                <div className={artiii.profileinfo}>
+                                <h1 >{article.Gender}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={artiii.lga}>
+                        <h1>LGA/STATE OF ORIGIN</h1>
+                        
+                        <div className={artiii.profileinfo}> 
+                        <div className={artiii.lgacol}>
+                                <h1 >- {article.State} -</h1>
+                                <h3>- {article.HometonCommunity} -</h3>
+                                </div>
+                                 </div>
+                            </div>
+                        
+                            <div className={artiii.bottom}>
+                            <div className={artiii.contact}>
+                                <h1>PARENT CONTACT 1:</h1>
+                                <div className={artiii.profileinfo}>
+                                    <>
+                                    <div className={artiii.p1}>
+                                            <p2>{article.ParentPhoneNo}</p2>
+                                        </div>
+                                    </>                   
+                                </div>
+                            </div>
+                            <div className={artiii.contact}>
+                                <h1>PARENT CONTACT 2:</h1>
+                                <div className={artiii.profileinfo}>
+                                    <>
+                                    <div className={artiii.p2}>
+                                            <p2>{article.ParentPhoneNo2}</p2>
+                                        </div>
+                                    </>                   
+                                </div>                           
+                            </div>
+                        </div>
+                    </div>       
+                </div>
+                </div>
+    </>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://www.mydatabase.com.ng/radical/${context.params.id}` )
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+      fallback:false
+    }
+   
+  }
+}
+
+export default article
